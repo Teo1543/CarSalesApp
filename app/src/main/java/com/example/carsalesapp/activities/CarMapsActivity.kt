65c7e46b00178d06ec4635d7f94b2abd3fcc531c
Teo Planinec <20093089@mail.wit.ie>
2023-04-09 14:57:09 +0100

    Map Activity working and displaying location.

--- app/src/main/java/com/example/carsalesapp/activities/CarMapsActivity.kt
+++ app/src/main/java/com/example/carsalesapp/activities/CarMapsActivity.kt
@@ -1,23 +1,85 @@
 package com.example.carsalesapp.activities
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.ui.AppBarConfiguration
+import com.example.carsalesapp.R
 import com.example.carsalesapp.databinding.ActivityCarMapsBinding
+import com.example.carsalesapp.databinding.ContentCarMapsBinding
+import com.example.carsalesapp.main.MainApp
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 
-class CarMapsActivity : AppCompatActivity() {
+class CarMapsActivity : AppCompatActivity(), GoogleMap.OnMarkerClickListener {
 
     private lateinit var binding: ActivityCarMapsBinding
+    private lateinit var contentBinding: ContentCarMapsBinding
+    lateinit var map: GoogleMap
+    lateinit var app: MainApp
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityCarMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         setSupportActionBar(binding.toolbar)
 
+        contentBinding = ContentCarMapsBinding.bind(binding.root)
+        contentBinding.mapView.onCreate(savedInstanceState)
+        app = application as MainApp
 
+        contentBinding.mapView.getMapAsync {
+            map = it
+            configureMap()
+        }
     }
+
+    private fun configureMap() {
+        map.uiSettings.isZoomControlsEnabled = true
+        app.cars.findAll().forEach {
+            val loc = LatLng(it.lat, it.lng)
+            val options = MarkerOptions().title(it.name).position(loc)
+            map.addMarker(options)?.tag = it.id
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, it.zoom))
+            map.setOnMarkerClickListener(this)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        contentBinding.mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        contentBinding.mapView.onLowMemory()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        contentBinding.mapView.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        contentBinding.mapView.onResume()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        contentBinding.mapView.onSaveInstanceState(outState)
+    }
+    override fun onMarkerClick(marker: Marker): Boolean {
+        contentBinding.currentTitle.text = marker.title
+
+        return false
+    }
+
 
 }
--- app/src/main/java/com/example/carsalesapp/models/CarStore.kt
+++ app/src/main/java/com/example/carsalesapp/models/CarStore.kt
@@ -5,6 +5,5 @@ interface CarStore {
     fun findAll(): List<CarModel>
     fun create(car: CarModel)
     fun update(car: CarModel)
-
-     fun delete(car: CarModel)
+    fun delete(car: CarModel)
 }
